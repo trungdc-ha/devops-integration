5bd56890bc61cc66787a039154c608933dc6a806
trungdc-ha <dangchitrungha@gmail.com>
2023-04-20 10:42:59 +0700

    TrungDC init project

---
+++ src/main/java/vn/codegym/DevopsIntegrationApplication.java
@@ -0,0 +1,17 @@
+package vn.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DevopsIntegrationApplication {
+    @GetMapping
+    public String message(){
+        return "welcome to TrungDC";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsIntegrationApplication.class, args);
+    }
+
+}
